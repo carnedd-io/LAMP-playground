e0af16adffe16842861d86e808b145b0e3392cd0
Edward Aslin <hello@edward.im>
2022-05-10 08:26:45 +0100

    PDO Connection practise

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
-// Create connection
-$conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"),getenv("DB_DATABASE"),getenv("FORWARD_DB_PORT"));
-
-// Check connection
-if ($conn->connect_error) {
-    die("DB Connection failed: " . $conn->connect_error);
+try {
+    new PDO("mysql:host=".getenv('DB_HOST').";port:".getenv("FORWARD_DB_PORT").";dbname=".getenv("DB_DATABASE")."", getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
+} catch(PDOException $e) {
+    print_r($e);
 }
-echo "DB Connection Success";
+
+//// Create connection
+//$conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"),getenv("DB_DATABASE"),getenv("FORWARD_DB_PORT"));
+//
+//// Check connection
+//if ($conn->connect_error) {
+//    die("DB Connection failed: " . $conn->connect_error);
+//}
+//echo "DB Connection Success";
 ?>
