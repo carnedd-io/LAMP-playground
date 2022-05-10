cd8b35abfc78f2389e88d749c7f1541027c71a31
Megan Jones <megan@carnedd.io>
2022-05-10 12:41:03 +0100

    Improved variable name for DB_FORWARD_PORT

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    new PDO("mysql:host=".getenv('DB_HOST').";port:".getenv("FORWARD_DB_PORT").";dbname=".getenv("DB_DATABASE")."", getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
+    new PDO("mysql:host=".getenv('DB_HOST').";port:".getenv("MYSQL_PORT").";dbname=".getenv("DB_DATABASE")."", getenv("DB_USERNAME"), getenv("DB_PASSWORD"));
 } catch(PDOException $e) {
     print_r($e);
 }
