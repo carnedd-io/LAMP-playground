990f75cdc4f892961088d8949a0b351616b9425b
Edward Aslin <hello@edward.im>
2022-05-10 08:29:33 +0100

    Removed depricated mysql connect

--- index.php
+++ index.php
@@ -6,12 +6,4 @@ try {
     print_r($e);
 }
 
-//// Create connection
-//$conn = mysqli_connect(getenv("DB_HOST"),getenv("DB_USERNAME"),getenv("DB_PASSWORD"),getenv("DB_DATABASE"),getenv("FORWARD_DB_PORT"));
-//
-//// Check connection
-//if ($conn->connect_error) {
-//    die("DB Connection failed: " . $conn->connect_error);
-//}
-//echo "DB Connection Success";
 ?>
